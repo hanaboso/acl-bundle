108ad27f086e31c5112830d72424bbe3f6e5d8cf
Radek Jirsa <jirsa.r@hanaboso.com>
2018-03-06 18:59:16 +0100

    RoleFixtures: fixed Entity namespaces

--- src/DataFixtures/ORM/RoleFixtures.php
+++ src/DataFixtures/ORM/RoleFixtures.php
@@ -4,10 +4,10 @@ namespace Hanaboso\AclBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use Hanaboso\AclBundle\Entity\Group;
-use Hanaboso\AclBundle\Entity\Rule;
+use Hanaboso\AclBundle\Entity\GroupInterface;
 use Hanaboso\AclBundle\Factory\MaskFactory;
-use Hanaboso\UserBundle\Entity\User;
+use Hanaboso\UserBundle\Exception\UserException;
+use Hanaboso\UserBundle\Provider\ResourceProvider;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Security\Core\Encoder\BCryptPasswordEncoder;
@@ -37,6 +37,8 @@ class RoleFixtures implements FixtureInterface, ContainerAwareInterface
      * Load data fixtures with the passed EntityManager
      *
      * @param ObjectManager $manager
+     *
+     * @throws UserException
      */
     public function load(ObjectManager $manager): void
     {
@@ -47,9 +49,16 @@ class RoleFixtures implements FixtureInterface, ContainerAwareInterface
         $encoder    = new BCryptPasswordEncoder(12);
         $rules      = $this->container->getParameter('acl_rule')['fixture_groups'];
         $ownerRules = $this->container->getParameter('acl_rule')['owner'];
+        $config     = $this->container->getParameter('db_res');
+        $enum       = $this->container->getParameter('resource_enum');
+
+        $provider   = new ResourceProvider($config);
+        $groupClass = $provider->getResource($enum::GROUP);
+        $userClass  = $provider->getResource($enum::USER);
+        $ruleClass  = $provider->getResource($enum::RULE);
 
         foreach ($rules as $key => $val) {
-            $group = new Group(NULL);
+            $group = new $groupClass(NULL);
             $group
                 ->setName($key)
                 ->setLevel($val['level']);
@@ -57,7 +66,7 @@ class RoleFixtures implements FixtureInterface, ContainerAwareInterface
 
             if (is_array($val['users'])) {
                 foreach ($val['users'] as $row) {
-                    $user = new User();
+                    $user = new $userClass();
                     $user
                         ->setPassword($encoder->encodePassword($row['password'], ''))
                         ->setEmail($row['email']);
@@ -67,12 +76,12 @@ class RoleFixtures implements FixtureInterface, ContainerAwareInterface
             }
             if (is_array($val['rules'])) {
                 foreach ($val['rules'] as $res => $rights) {
-                    $this->createRule($manager, $group, $rights, $res);
+                    $this->createRule($manager, $group, $rights, $res, $ruleClass);
                 }
             }
             if (is_array($ownerRules)) {
                 foreach ($ownerRules as $res => $rights) {
-                    $this->createRule($manager, $group, $rights, $res);
+                    $this->createRule($manager, $group, $rights, $res, $ruleClass);
                 }
             }
 
@@ -82,14 +91,21 @@ class RoleFixtures implements FixtureInterface, ContainerAwareInterface
     }
 
     /**
-     * @param ObjectManager $manager
-     * @param Group         $group
-     * @param array         $rights
-     * @param string        $res
+     * @param ObjectManager  $manager
+     * @param GroupInterface $group
+     * @param array          $rights
+     * @param string         $res
+     * @param string         $ruleClass
      */
-    private function createRule(ObjectManager $manager, Group $group, array $rights, string $res): void
+    private function createRule(
+        ObjectManager $manager,
+        GroupInterface $group,
+        array $rights,
+        string $res,
+        string $ruleClass
+    ): void
     {
-        $rule = new Rule();
+        $rule = new $ruleClass();
         $rule
             ->setGroup($group)
             ->setActionMask(MaskFactory::maskActionFromYmlArray($rights))
