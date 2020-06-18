b6a45f59a7819f31ecd028bf4d4578fb8f0c4e08
David Horacek <horacek.d@hanaboso.com>
2020-06-18 08:20:06 +0200

    composer update

--- src/Repository/Document/RuleRepository.php
+++ src/Repository/Document/RuleRepository.php
@@ -3,13 +3,14 @@
 namespace Hanaboso\AclBundle\Repository\Document;
 
 use Doctrine\ODM\MongoDB\Repository\DocumentRepository;
+use Hanaboso\AclBundle\Document\Rule;
 
 /**
  * Class RuleRepository
  *
  * @package Hanaboso\AclBundle\Repository\Document
  *
- * @phpstan-extends DocumentRepository<\Hanaboso\AclBundle\Document\Rule>
+ * @phpstan-extends DocumentRepository<Rule>
  */
 class RuleRepository extends DocumentRepository
 {
